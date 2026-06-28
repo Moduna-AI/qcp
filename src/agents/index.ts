23bb68c18ff3a190ea49ea16cb803fbe6e21ec9f
Ashwin Thinnappan <sjashwin@live.in>
2026-06-28 21:41:32 +0530

    (chore:abstract): Added Abstract Class Database

---
+++ src/agents/index.ts
@@ -0,0 +1,13 @@
+export {
+	AbstractDatabaseAgent,
+	type DatabaseAgentConfig,
+	type DatabaseAgentType,
+} from "./database-agent.js";
+export { NeonAgent, type NeonAgentConfig } from "./neon-agent.js";
+export {
+	OraclePostgresAgent,
+	type OraclePostgresAgentConfig,
+} from "./oracle-postgres-agent.js";
+export { PostgresAgent, type PostgresAgentConfig } from "./postgres-agent.js";
+export { PrismaAgent, type PrismaAgentConfig } from "./prisma-agent.js";
+export { SupabaseAgent, type SupabaseAgentConfig } from "./supabase-agent.js";
